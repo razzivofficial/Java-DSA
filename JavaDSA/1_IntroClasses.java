dc925ee4d50292ae922c1fd64afe25a786670f5b
razzivofficial <razzivofficial@gmail.com>
2023-09-26 20:29:41 +0530

    restructuring

---
+++ JavaDSA/1_IntroClasses.java
@@ -0,0 +1,8 @@
+package JavaDSA;
+import java.util.*;
+
+class IntroClasses {
+    public static void main( String []args ) {
+        System.out.println( "Jay Shree Ram" );
+    }
+}
--- JavaOOPS/1_IntroClasses.java
+++
@@ -1,8 +0,0 @@
-package JavaOOPS;
-import java.util.*;
-
-class IntroClasses {
-    public static void main( String []args ) {
-        System.out.println( "Jay Shree Ram" );
-    }
-}
